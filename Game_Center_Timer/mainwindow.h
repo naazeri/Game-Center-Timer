cbdb1df190dea568cf78ef1f984cde36284cac22
IRRN <reza.armani75@rayana.ir>
2015-07-13 19:10:09 +0430

    write project

--- Game_Center_Timer/mainwindow.h
+++ Game_Center_Timer/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QTime>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +16,21 @@ class MainWindow : public QMainWindow
 public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
+	void calculatePrice();
+
+private slots:
+	void on_endBtn_clicked();
+	void on_startBtn_clicked(bool checked);
+	void updateTimer();
 
 private:
 	Ui::MainWindow *ui;
+	QTimer *timer;
+	QTime *time;
+	qint64 count;
+	const float price1 = 0.694;
+	const float price2 = 0.833;
+	bool stop;
 };
 
 #endif // MAINWINDOW_H
