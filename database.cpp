19c151f1834f9c0073f9351c125b562077760727
IRRN <reza.armani75@rayana.ir>
2015-07-15 00:35:03 +0430

    add variable, signals, slots

--- database.cpp
+++ database.cpp
@@ -2,7 +2,7 @@
 
 DataBase::DataBase(QObject *parent) : QObject(parent)
 {
-
+	initData();
 }
 
 DataBase::~DataBase()
@@ -10,3 +10,52 @@ DataBase::~DataBase()
 
 }
 
+void DataBase::initData()
+{
+	timer = new QTimer(this);
+	time = new QTime();
+	time->setHMS(0,0,0,0);
+
+	isSp = true;
+	thePayment = 0;
+	spentTime = 0;
+	spPerSec = 0.6;
+	mpPerSec = 0.8;
+
+	connect(timer, SIGNAL(timeout()), this, SLOT());
+}
+
+int DataBase::payment()
+{
+	return thePayment;
+}
+
+void DataBase::setPayment(int payment)
+{
+	thePayment = payment;
+}
+
+void DataBase::start()
+{
+
+}
+
+void DataBase::pause()
+{
+
+}
+
+void DataBase::stop()
+{
+
+}
+
+void DataBase::spActivated()
+{
+
+}
+
+void DataBase::mpActivated()
+{
+
+}
--- database.h
+++ database.h
@@ -2,17 +2,39 @@
 #define DATABASE_H
 
 #include <QObject>
+#include <QTimer>
+#include <QTime>
 
 class DataBase : public QObject
 {
 	Q_OBJECT
+	Q_PROPERTY(int payment READ payment WRITE setPayment NOTIFY paymentChanged)
+
 public:
 	explicit DataBase(QObject *parent = 0);
 	~DataBase();
+	void initData();
+	int payment();
+	void setPayment(int payment);
+
+private:
+	bool isSp;
+	int thePayment;
+	qint64 spentTime;
+	float spPerSec, mpPerSec;
+	QTime *time;
+	QTimer *timer;
 
 signals:
+	void paymentChanged();
 
 public slots:
+	void start();
+	void pause();
+	void stop();
+	void spActivated();
+	void mpActivated();
+
 };
 
 #endif // DATABASE_H
